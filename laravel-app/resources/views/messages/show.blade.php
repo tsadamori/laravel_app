18b9f67cde261e1881f099454ddb0ff4c240c42a
貞森友章 <sadamori@MacBook-Pro.local>
2020-09-14 08:58:38 +0900

    CRUD messages

---
+++ laravel-app/resources/views/messages/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>id = {{ $message->id }} のメッセージ詳細ページ</h1>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $message->content }}</td>
+        </tr>
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $message->content }}</td>
+        </tr>
+    </table>
+
+    {!! link_to_route('messages.edit', 'このメッセージを編集', $message->id, ['class' => 'btn btn-light']) !!}
+
+    {!! Form::model($message, ['route' =>['message.destroy', $message->id], 'method' =>'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
+@endsection
--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MessagesController@index');
+Route::resource('messages', 'MessagesController');
+
+// Route::get('messages/{id}', 'MessagesController@show');
+// Route::post('messages', 'MessagesController@store');
+// Route::put('messages/{id}', 'MessagesController@update');
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// Route::get('messages', 'MessagesController@index')->name('messages.index');
+// Route::get('messages/create', 'MessagesController@create')->name('messages.create');
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
