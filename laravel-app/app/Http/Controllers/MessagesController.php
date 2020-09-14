fe77ff02ab402efaa1c63e71004b654b9a9ae6d6
貞森友章 <sadamori@MacBook-Pro.local>
2020-09-14 09:12:01 +0900

    validation

--- laravel-app/app/Http/Controllers/MessagesController.php
+++ laravel-app/app/Http/Controllers/MessagesController.php
@@ -44,6 +44,10 @@ class MessagesController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191'
+        ]);
+
         $message = new Message;
         $message->content = $request->content;
         $message->save();
@@ -91,6 +95,10 @@ class MessagesController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+
         $message = Message::find($id);
         $message->content = $request->content;
         $message->save();
